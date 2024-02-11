f6ed001c4d7a9fa784ecf349fb403d707bfbadc8
creature6969 <159709673+creature6969@users.noreply.github.com>
2024-02-11 20:53:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/tinaarobot/ROYMUSIC",
+    "https://github.com/creature6969/pvt",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
