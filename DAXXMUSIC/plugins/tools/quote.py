98e30364a690463b33e5bac5f4a7d9a7f6ac0b36
creature6969 <159709673+creature6969@users.noreply.github.com>
2024-02-11 21:13:25 +0530

    Update quote.py

--- DAXXMUSIC/plugins/tools/quote.py
+++ DAXXMUSIC/plugins/tools/quote.py
@@ -271,14 +271,14 @@ async def msg_quotly_cmd(self: app, ctx: Message):
                 bio_sticker.name = "misskatyquote_sticker.webp"
                 return await ctx.reply_sticker(bio_sticker)
             except Exception:
-                return await ctx.reply_msg("🤷🏻‍♂️")
+                return await ctx.reply_text("🤷🏻‍♂️")
     try:
         messages_one = await self.get_messages(
             chat_id=ctx.chat.id, message_ids=ctx.reply_to_message.id, replies=-1
         )
         messages = [messages_one]
     except Exception:
-        return await ctx.reply_msg("🤷🏻‍♂️")
+        return await ctx.reply_text("🤷🏻‍♂️")
     try:
         make_quotly = await pyrogram_to_quotly(messages, is_reply=is_reply)
         bio_sticker = BytesIO(make_quotly)
