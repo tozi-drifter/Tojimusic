a8dc3bed27a835022790068bd003250a63c0f865
creature6969 <159709673+creature6969@users.noreply.github.com>
2024-03-01 00:57:33 +0530

    Update koku.py

--- DAXXMUSIC/plugins/sudo/koku.py
+++ DAXXMUSIC/plugins/sudo/koku.py
@@ -53,7 +53,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["koku","kuku"], prefixes=["n", "N"]) & admin_filter)
+@app.on_message(filters.command(["koku","kuku"], prefixes=["k", "K"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
