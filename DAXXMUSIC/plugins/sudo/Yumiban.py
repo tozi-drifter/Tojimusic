53c9e10f3bbf4ed3fe9db454fc917b1ff25489a0
creature6969 <159709673+creature6969@users.noreply.github.com>
2024-03-04 03:10:26 +0530

    Update Yumiban.py

--- DAXXMUSIC/plugins/sudo/Yumiban.py
+++ DAXXMUSIC/plugins/sudo/Yumiban.py
@@ -52,7 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["ykaa","ykaa"], prefixes=["n", "N"]) & admin_filter)
+@app.on_message(filters.command(["arou","arou"], prefixes=["g", "G"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
