0020669f4ffa101ca5b4afecaa81d8fdef9c664b
tozi-drifter <154064345+tozi-drifter@users.noreply.github.com>
2024-03-13 04:42:19 +0530

    Update Gn.py

--- DAXXMUSIC/plugins/tools/Gn.py
+++ DAXXMUSIC/plugins/tools/Gn.py
@@ -20,8 +20,8 @@ def goodnight_command_handler(_, message):
 
 def get_random_sticker():
     stickers = [
-        "CAACAgQAAx0Ce9_hCAACaEVlwn7HeZhgwyVfKHc3WUGC_447IAACLgwAAkQwKVPtub8VAR018x4E", # Sticker 1
-        "CAACAgIAAx0Ce9_hCAACaEplwn7dvj7G0-a1v3wlbN281RMX2QACUgwAAligOUoi7DhLVTsNsh4E", # Sticker 2
+        "CAACAgUAAxkBAAI9hmXw2tzh9xyHRSljDUQQsIjd0IX-AAJkCAAC0vBpVtXQG48anAPxHgQ", # Sticker 1
+        "CAACAgUAAxkBAAI9i2Xw2wxG0CM6xcJRzA2OiOcAAZdiMwACSwcAAvqZWVT5N90fK8dgEx4E", # Sticker 2
         "CAACAgIAAx0Ce9_hCAACaFBlwn8AAZNB9mOUvz5oAyM7CT-5pjAAAtEKAALa7NhLvbTGyDLbe1IeBA", # Sticker 3
         "CAACAgUAAx0CcmOuMwACldVlwn9ZHHF2-S-CuMSYabwwtVGC3AACOAkAAoqR2VYDjyK6OOr_Px4E",
         "CAACAgIAAx0Ce9_hCAACaFVlwn-fG58GKoEmmZpVovxEj4PodAACfwwAAqozQUrt2xSTf5Ac4h4E",
