b2ee42e407b441feb3ff7afcf6200ea501e87255
tozi-drifter <154064345+tozi-drifter@users.noreply.github.com>
2024-04-07 19:08:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/tozi-drifter/music",
+    "https://github.com/tozi-drifter/Tojimusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
